d6aa5c47689ea20c432709829c4fd9986b787e9e
Luke Vaughan <lukeevaughan@gmail.com>
2024-03-15 02:14:17 +0000

    created and implemented attributes on comments table

---
+++ database/migrations/2024_03_15_021102_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('postID')->unsigned();
+            $table->integer('userID')->unsigned();
+            $table->string('content');
+            $table->timestamps();
+
+            $table->foreign('postID')->references('id')->on('posts');
+            $table->foreign('userID')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
